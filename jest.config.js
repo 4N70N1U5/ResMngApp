ded992c974378e3f1f4737240bb64510e7992ae1
Sabau Eduard <59087888+SabauEduard@users.noreply.github.com>
2024-11-10 17:25:32 +0200

    save results

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './test-results',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
+};
